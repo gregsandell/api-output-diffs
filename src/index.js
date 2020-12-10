f86dc5c29555293f20a6929c88441db0b042c923
Greg Sandell <gsandell_99@yahoo.com>
2020-12-09 18:06:54 -0800

    Remove comments

--- src/index.js
+++ src/index.js
@@ -1,29 +1,10 @@
 #!/usr/local/bin/node
-// console.time('Input files loaded in')
+
 const mockStore = require('../data/mockStore.js').data
 const prodStore = require('../data/prodStore.js').data
 const { StoreSolver } = require('./storeSolver.js')
-// console.timeEnd('Input files loaded in')
 
 const solver = new StoreSolver(mockStore, prodStore)
 const output = solver.solve()
 console.log(solver.report())
 solver.promptForOutput(output).then(() => process.exit(0))
-// console.log(JSON.stringify(mockStore))
-/**
- * Find the difference between two store API outputs.
- *
- * searching specifically for the ids of all stores that are not in the other,
- * as well as the fields from the mock API that are not
- * present in the production one.
- *
- * Example return format
- * {
- *   stores: [
- *     { storeId: 'XXXXX', missingFrom: 'prod || mock' },
- *     ...
- *  ],
- *  fields: [ 'field1', 'field2', ... ]
- * }
- *
- */
